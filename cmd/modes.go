5c6afd2b7a73571aa2a2c7d86d944ec5742fa8fe
Neil Mahajan <neilsmahajan@gmail.com>
2025-10-05 16:56:25 -0400

    Add modes command

---
+++ cmd/modes.go
@@ -0,0 +1,22 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var modesCmd = &cobra.Command{
+	Use:   "modes",
+	Short: "List supported modes",
+	Long:  `List all the modes supported by the typing test application.`,
+	Run:   listModes,
+}
+
+func listModes(cmd *cobra.Command, args []string) {
+	cmd.Println("Supported Modes:")
+	cmd.Println(" - quote : Type predefined quotes.")
+	cmd.Println(" - words : Type a set number of random words.")
+	cmd.Println(" - time  : Type as many words as you can in a set time limit.")
+	cmd.Println("\nYou can specify a mode using the --mode or -m flag when starting a typing test.")
+}
+
+func init() {
+	rootCmd.AddCommand(modesCmd)
+}
